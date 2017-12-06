8e03ff031ed5e236ec55c6a6ef5f2ec8348c00d7
bobjrsenior <bobjrsenior@gmail.com>
2017-12-06 18:30:12 -0500

    Day 6 part 2

--- Day6/Day6/main.c
+++ Day6/Day6/main.c
@@ -15,7 +15,7 @@ int equalMemory(const uint8_t *memoryOne, const uint8_t *memoryTwo) {
 int historyIncludes(uint8_t blockHistory[][16], int historyCount, const uint8_t *blocks) {
 	for (int i = 0; i < historyCount; i++) {
 		if (equalMemory(blockHistory[i], blocks)) {
-			return 1;
+			return i;
 		}
 	}
 	return 0;
@@ -32,7 +32,7 @@ void partOne(char *filename) {
 	FILE *input = fopen(filename, "r");
 	if (input == NULL) {
 		puts("File not found");
-		return -1;
+		return;
 	}
 
 	int numBlocks = 0;
@@ -80,12 +80,66 @@ void partOne(char *filename) {
 	printf("Num Distributions: %d\n", historyCount);
 }
 
+void partTwo(char *filename) {
+	// Open file
+	FILE *input = fopen(filename, "r");
+	if (input == NULL) {
+		puts("File not found");
+		return;
+	}
+
+	int numBlocks = 0;
+	uint8_t blocks[32] = { 0 };
+	// Fill blocks
+	while (!feof(input)) {
+		if (fscanf(input, "%" PRIu8, &blocks[numBlocks]) != 1) {
+			break;
+		}
+		numBlocks++;
+	}
+	fclose(input);
+
+	int historyCount = 1;
+	uint8_t blockHistory[16000][16] = { 0 };
+	writeHistory(blockHistory, 0, blocks);
+
+	while (1) {
+		// Find max block
+		int maxIndex = 0;
+		for (int i = 1; i < numBlocks; i++) {
+			if (blocks[i] > blocks[maxIndex]) {
+				maxIndex = i;
+			}
+		}
+
+		// Distribute
+		int maxValue = blocks[maxIndex];
+		blocks[maxIndex] = 0;
+		for (int i = 1; i < maxValue + 1; i++) {
+			blocks[(maxIndex + i) % numBlocks]++;
+		}
+
+		int result = historyIncludes(blockHistory, historyCount, blocks);
+		if (result) {
+			printf("Length of Loop: %d\n", historyCount - result);
+			break;
+		}
+		writeHistory(blockHistory, historyCount, blocks);
+		historyCount++;
+
+		if (historyCount == 16000) {
+			break;
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		return 0;
 	}
 
 	partOne(argv[1]);
+	partTwo(argv[1]);
 
 	return 0;
 }
